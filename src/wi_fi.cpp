9f097f409f8cb30871bab2f095c8ca9dd9798c84
magikfly <98903923+magikfly@users.noreply.github.com>
2025-05-24 10:05:15 +0200

    Update wi_fi.cpp

--- src/wi_fi.cpp
+++ src/wi_fi.cpp
@@ -5,7 +5,7 @@
 
 void WiFiManager::connect() {
   WiFi.mode(WIFI_STA);
-  WiFi.begin("homenet_24ghz", "euadoromuftalinar");
+  WiFi.begin("wifi", "pass");
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
